8c414e327cac3d5d302ef19f04828f20106d96bb
robsongermano <germanodi@hotmail.com>
2022-07-07 13:38:15 -0300

    Adicionar arquivos de projeto.

---
+++ tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int formula, contador, numero;
+            Console.WriteLine("\n\n************ tabuada ****************");
+            Console.WriteLine("\nDigite um  numero para obter a tabuada :");
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            for (contador = 1; contador <= 10; ++contador)
+
+            {
+                formula = numero * contador;
+                Console.WriteLine(numero + " x " + contador + " = " + formula);
+
+            }
+           
+        }
+    }
+}
